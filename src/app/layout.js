1bc626a250444198265de272605fea96ee3bd0b7
kaiodegras <kaioa9851@gmail.com>
2024-02-21 17:19:38 -0400

    Atividade PTAC3

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import Menu from "../componentes/Menu";
+import Footer from "../componentes/footer";
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -7,7 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        <Menu/>
+        {children}
+        <Footer/>
+      </body>
+      
     </html>
   )
 }
