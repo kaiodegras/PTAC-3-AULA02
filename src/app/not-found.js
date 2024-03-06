a39948f7b225a34a3ee8eb4665028c429095a940
kaiodegras <kaioa9851@gmail.com>
2024-03-06 09:25:48 -0400

    Atividade PTAC

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,12 +2,11 @@ import Link from "next/link";
 export default function NoFound() {
    
   return (
-    <div>
+    
 
      <h1>Error!</h1>
-     <Link href="/"></Link>
+    
      
-    </div> 
   
   );
 }
